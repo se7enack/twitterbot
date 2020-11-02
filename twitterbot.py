858452d4e170d8d353a684b8dc04099d74c612a7
Stephen Burke <se7enack@users.noreply.github.com>
2020-11-02 11:01:43 -0500

    Update twitterbot.py

--- twitterbot.py
+++ twitterbot.py
@@ -1,23 +1,25 @@
 import tweepy
 import time
+from datetime import datetime
 
 # https://developer.twitter.com/en/apps
 auth = tweepy.OAuthHandler('API_key','API_secret_key')
 auth.set_access_token('Access_token','Access_token_secret')
 
-api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 user = api.me()
-search = "Python"
+search = input("Please enter hashtag string: ")
 numberOfTweets = 365250
 
 for tweet in tweepy.Cursor(api.search, search).items(numberOfTweets):
     try:
         print('Retweeted count: {0}'.format(tweet.retweet_count))
-        if tweet.retweet_count < 1:
+        #if api.verify_credentials() == False:
+        if 1 <= tweet.retweet_count <= 5:
             tweet.retweet()
+            #tweet.favorite()
+            now = datetime.now()
+            print(now)
             print("https://twitter.com/" + str(tweet.id) + "/status/" + str(tweet.id))
             time.sleep(87)
-            # tweet.favorite()
-            # print('Tweet Liked')
     except tweepy.TweepError as e:
         print(e)
