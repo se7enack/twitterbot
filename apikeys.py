fa77336fcd98418666c8df3a1b043b590d70cc22
Stephen Burke <se7enack@users.noreply.github.com>
2020-11-03 13:04:26 -0500

    Create apikeys.py

---
+++ apikeys.py
@@ -0,0 +1,7 @@
+# https://developer.twitter.com/en/apps
+apikeys = dict(
+    api_key='API_key',
+    api_secret_key='API_secret_key',
+    access_token='Access_token',
+    access_token_secret='Access_token_secret',
+)
